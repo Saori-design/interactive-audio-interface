d22117bdf9326d3ad2e6aa16afc38e907896950a
Saori-design <65802428+Saori-design@users.noreply.github.com>
2020-12-20 01:41:39 -0500

    code clean up

--- js/script.js
+++ js/script.js
@@ -1,92 +1,75 @@
 let musicContainer = document.getElementById('music-container');
-
 let playBtn = document.getElementById('play');
 let prevBtn = document.getElementById('prev');
 let nextBtn = document.getElementById('next');
-
 let audio = document.getElementById('audio');
 let titleContainer = document.getElementById('title-container');
 let title = document.getElementById('title');
 let cover = document.getElementById('cover');
 
 //song titles array
-let songs = ['creativeminds', 'dance', 'funday', 'onceagain', 'punky'];
+const SONGS = ['creative_minds', 'dance', 'funday', 'once_again', 'punky'];
+const LAST_SONG_INDEX = SONGS.length - 1;
 
-//track of song
+// current index of song
 let songIndex = 0;
 
-//Load song details into DOM
-loadSong(songs[songIndex]);
+let playSong = function () {
+	musicContainer.classList.add('play');
+	playBtn.querySelector('span.fas').classList.remove('fa-play');
+	playBtn.querySelector('span.fas').classList.add('fa-pause');
+	audio.play();
+};
 
-//Update song details
+let pauseSong = function () {
+	musicContainer.classList.remove('play');
+	playBtn.querySelector('span.fas').classList.add('fa-play');
+	playBtn.querySelector('span.fas').classList.remove('fa-pause');
+	audio.pause();
+};
+
+let prevSong = function () {
+	songIndex--;
+	if (songIndex < 0) {
+		songIndex = LAST_SONG_INDEX;
+	}
+
+	loadSong(SONGS[songIndex]);
+	playSong();
+};
+
+let nextSong = function () {
+	songIndex++;
+	if (songIndex > LAST_SONG_INDEX) {
+		songIndex = 0;
+	}
+
+	loadSong(SONGS[songIndex]);
+	playSong();
+};
+
 function loadSong(song) {
 	title.innerText = song;
 	audio.src = `./audio/${song}.mp3`;
 	cover.src = `./img/${song}.jpg`;
-	playSong();
 }
 
-	
-//Play song
-function playSong() {
-	musicContainer.classList.add('play');
-	playBtn.querySelector('i.fas').classList.remove('fa-play');
-	playBtn.querySelector('i.fas').classList.add('fa-pause');
-	// alert("src=" + playSong);
-	audio.play();
-}
-
-//Pause song
-function pauseSong() {
-	musicContainer.classList.remove('play');
-	playBtn.querySelector('i.fas').classList.add('fa-play');
-	playBtn.querySelector('i.fas').classList.remove('fa-pause');
-	// alert("src=" + playSong);
-	audio.pause();
-}
-
-
-
-
-//Previous song
-let prevSong = function (){
-	songIndex --;
-
-	if(songIndex < 0) {
-		songIndex = songs.length - 1;
-	}
-
-	loadSong(songs[songIndex]);
-
-	playSong();
-}
-
-//Next Song 
-let nextSong = function (){
-	songIndex ++;
-
-	if(songIndex > songs.length - 1) {
-	songIndex = 0;
-	}
-	loadSong(songs[songIndex]);
-
-	//playSong();
-}
-
-//Event listener
+// Click Listeners
 playBtn.addEventListener('click', function() {
 	let isPlaying = musicContainer.classList.contains('play');
 	
-	if(isPlaying) {
-	pauseSong();
+	if (isPlaying) {
+		pauseSong();
 	} else {
-	playSong();
+		playSong();
 	}
 });
 
-//Change song
-prevBtn.addEventListener('click',prevSong)
-nextBtn.addEventListener('click',nextSong)
+prevBtn.addEventListener('click',prevSong);
+nextBtn.addEventListener('click',nextSong);
 
-//Song end
-audio. addEventListener('ended', nextSong)
+// Song end
+audio.addEventListener('ended', nextSong);
+
+// Load song details into DOM when document loads
+loadSong(SONGS[songIndex]);
